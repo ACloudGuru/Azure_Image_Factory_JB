447309034df4d338ba394d3a96126d87cdbad54f
James Bannan <james@bannan.com.au>
2018-06-05 17:22:54 +1000

    Change Resource Group script tag handling

--- scripts/createResourceGroup.ps1
+++ scripts/createResourceGroup.ps1
@@ -4,15 +4,21 @@ param(
     [string]$ResourceGroupName,
     [Parameter(Mandatory=$True)]
     [string]$ResourceGroupLocation,
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory=$false)]
     [array]$ResourceGroupTags
 )
 
 $ResourceGroupLocation = $ResourceGroupLocation.ToLower().Replace(' ','')
 
 if((az group exists --name $ResourceGroupName) -ne $True){
-    $resourceGroup = az group create --name $ResourceGroupName --location $ResourceGroupLocation --tags $ResourceGroupTags --verbose | ConvertFrom-Json
-    Write-Host $resourceGroup
+    if($ResourceGroupTags -ne $null){
+        $resourceGroup = az group create --name $ResourceGroupName --location $ResourceGroupLocation --tags $ResourceGroupTags --verbose | ConvertFrom-Json
+        Write-Host $resourceGroup
+    }
+    else {
+        $resourceGroup = az group create --name $ResourceGroupName --location $ResourceGroupLocation --verbose | ConvertFrom-Json
+        Write-Host $resourceGroup
+    }
 }
 else {
     $resourceGroup = az group show --name $ResourceGroupName | ConvertFrom-Json
